9b3109e6330a12b88c85f87f721505bda61f4e13
unknown <antonafonin9@gmail.com>
2022-04-08 17:12:27 +0300

    Adding changes

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { createElement, useEffect, useState } from 'react';
 import { Container, Grid, TextField, Button, Avatar } from '@mui/material';
-import { saveTask, loadMessages } from '../posts';
-import {useCollectionData} from "react-firebase-hooks/firestore";
+import { saveTask, loadTasks } from '../posts';
+import { query, collection, getDocs, limit, orderBy, getFirestore, onSnapshot } from 'firebase/firestore';
+import '../App.css';
 
 const Posts = () => {
 
+    loadTasks();
+    
     const [task, setTask] = useState("");
     const [date, setDate] = useState("");
 
@@ -13,16 +16,40 @@ const Posts = () => {
         setTask("");
         setDate("");
     }
-    const [messages, loading] = useCollectionData(
-        firestore.collection('messages').orderBy('createdAt')
-    )
 
+    
+    // async function loadTasks() {
+    //     const q = query(collection(getFirestore(), 'tasks'), orderBy('timestamp', 'desc'), limit(12));
+    //     var array = [];
+    //     onSnapshot(q, function(snap) {
+    //         snap.docChanges().forEach(function(change) {
+    //             var data = change.doc.data()
+    //             createTask(data.text, data.deadline)
+    //         })
+    //     })
+    // }
+
+    // function createTask(task, deadline) {
+    //     let root = document.getElementById("chat-area")
+    //     let container = document.createElement("Grid")
+    //     container.className = "task-container"
+
+    //     let newTask = document.createElement("div");
+    //     newTask.innerHTML = task;
+    //     container.appendChild(newTask);
+
+    //     let newDeadline = document.createElement("div");
+    //     newDeadline.innerHTML = "Deadline: " + deadline;
+    //     container.appendChild(newDeadline);
+
+    //     root.appendChild(container)
+    // }
 
     return (
         <Container>
             <Grid container justify={"center"} style={{ height: window.innerHeight - 70, marginTop: 10, border: "1px solid grey" }}>
                 <div id="chat-area" style={{ height: "90%", width: "100%", border: '1px solid gray', overflowY: 'auto' }}>
-
+                    
                 </div>
                 <Grid container direction={"row"} justifyContent={"space-around"} >
                     <TextField label="What" variant={"outlined"} style={{ width: "30%" }} onChange={(e) => setTask(e.target.value)}></TextField>
--- src/posts.js
+++ src/posts.js
@@ -11,7 +11,8 @@ import {
     updateDoc,
     doc,
     serverTimestamp,
-    getDocs
+    getDocs,
+    QuerySnapshot
 } from 'firebase/firestore';
 import { getProfilePicUrl, getUserName } from './auth'
 import { Grid, Avatar } from '@mui/material';
@@ -33,34 +34,105 @@ export async function saveTask(taskText, taskDeadline) {
 }
 
 
-export async function loadMessages() {
-    // Create the query to load the last 12 messages and listen for new ones.
-    const recentTasksQuery = query(collection(getFirestore(), 'tasks'), orderBy('timestamp', 'desc'), limit(12));
-    const querySnapshot = await getDocs(recentTasksQuery);
-    const div = document.getElementById("chat-area")
+
+
+var messageListElement = document.getElementById('chat-area');
+
+function displayMessage(id, timestamp, name, text, picUrl, imageUrl, deadline) {
+    var div =
+      document.getElementById(id) || createAndInsertMessage(id, timestamp);
+  
+    // profile picture
+    if (picUrl) {
+      div.querySelector('.pic').style.backgroundImage =
+        'url(' + picUrl + ')';
+    }
+  
+    div.querySelector('.name').textContent = name;
+    var messageElement = div.querySelector('.message');
+    var deadlineElement = div.querySelector('.deadline')
+  
+    if (text) {
+      // If the message is text.
+      messageElement.textContent = text;
+      deadlineElement.textContent = deadline;
+      // Replace all line breaks by <br>.
+      messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
+    } else if (imageUrl) {
+      // If the message is an image.
+      var image = document.createElement('img');
+      image.addEventListener('load', function () {
+        messageListElement.scrollTop = messageListElement.scrollHeight;
+      });
+      image.src = imageUrl + '&' + new Date().getTime();
+      messageElement.innerHTML = '';
+      messageElement.appendChild(image);
+    }
+    // Show the card fading-in and scroll to view the new message.
+    setTimeout(function () {
+      div.classList.add('visible');
+    }, 1);
+    messageListElement.scrollTop = messageListElement.scrollHeight;
+    }
+
+  var MESSAGE_TEMPLATE =
+  '<div class="message-container">' +
+  '<div class="spacing"><div class="pic"></div></div>' +
+  '<div class="name"></div>' +
+  '<div class="task-info">'+
+  '<div class="message"></div>' +
+  '<div class="deadline"></div>' +
+  '</div>'+
+  '<Button class="doneButton">Done</Button>' +
+  '</div>';
+
+  function createAndInsertMessage(id, timestamp) {
+    const container = document.createElement('div');
+    container.innerHTML = MESSAGE_TEMPLATE;
+    const div = container.firstChild;
+    div.setAttribute('id', id);
+  
+    // If timestamp is null, assume we've gotten a brand new message.
+    // https://stackoverflow.com/a/47781432/4816918
+    timestamp = timestamp ? timestamp.toMillis() : Date.now();
+    div.setAttribute('timestamp', timestamp);
+  
+    // figure out where to insert new message
+    messageListElement = document.getElementById('chat-area');
+    const existingMessages = messageListElement.children;
+    if (existingMessages.length === 0) {
+      messageListElement.appendChild(div);
+    } else {
+      let messageListNode = existingMessages[0];
+  
+      while (messageListNode) {
+        const messageListNodeTime = messageListNode.getAttribute('timestamp');
+  
+        if (!messageListNodeTime) {
+          throw new Error(
+            `Child ${messageListNode.id} has no 'timestamp' attribute`
+          );
+        }
+  
+        if (messageListNodeTime > timestamp) {
+          break;
+        }
+  
+        messageListNode = messageListNode.nextSibling;
+      }
+  
+      messageListElement.insertBefore(div, messageListNode);
+    }
+  
+    return div;
+  }
+
+  export async function loadTasks() {
+    const q = query(collection(getFirestore(), 'tasks'), orderBy('timestamp', 'desc'), limit(12));
+    const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        // displayMessage(doc.data().timestamp, doc.data().name, doc.data().text, doc.data().deadline, doc.data().profilePicUrl)
-        console.log(doc.data())
+        let myData = doc.data();
+        displayMessage(doc.id, myData.timestamp, myData.name, myData.text, myData.profilePicUrl, myData.profilePicUrl, myData.deadline)
     });
 }
-
-const pushToArray = () => {
-
-}
-
-function displayMessage(timestamp, name, text, deadline, picUrl) {
-    return <div style={{ margin: 10, padding: 3, border: '1px solid grey' }}>
-        <Grid container >
-            <Avatar src={picUrl} />
-            <div>{name}</div>
-        </Grid>
-        <div>Task: {text}</div>
-        <div>Deadline: {deadline}</div>
-        <div>Created: {timestamp}</div>
-    </div>
-}
-
-// function deleteMessage() {
-
-// }
